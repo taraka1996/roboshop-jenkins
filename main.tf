78911cec57e7004b788e894503a36f13c3174e94
TARAK <tarakaramtirumala@gmail.com>
2023-04-08 10:33:47 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 resource "jenkins_folder" "folders" {
-  name = "infrastructure"
+  count = length(var.folders)
+  name  = element(var.folders, count.index)
 }
 
 resource "jenkins_job" "job" {
-    name = "roboshop"
-    folder = jenkins_folder.folders.id
-    template = templatefile("${path.module}/sb-job.xml", {
-      description = "An example job created from terraform"
-    })
+  depends_on = [jenkins_folder.folders]
+
+  count  = length(var.jobs)
+  name   = lookup(element(var.jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
+
+  template = templatefile("${path.module}/sb-job.xml", {
+    repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.jobs, count.index), "name", null)
+  })
 }
