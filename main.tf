6e841f7e8c7657da7bb928e1829342af501c7758
TARAK <tarakaramtirumala@gmail.com>
2023-04-08 10:16:58 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "jenkins_folder" "folders" {
   name = "infrastructure"
 }
 
-resource "jenkins_job" "jobs" {
+resource "jenkins_job" "job" {
     name = "roboshop"
     folder = jenkins_folder.folders.id
     template = templatefile("${path.module}/sb-job.xml", {
