ccd13846ab55a17fa16079dc3df55e8e08634d7c
TARAK <tarakaramtirumala@gmail.com>
2023-04-08 10:22:35 +0530

    changes

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "jenkins_job" "job" {
     name = "roboshop"
     folder = jenkins_folder.folders.id
     template = templatefile("${path.module}/sb-job.xml", {
-        description = "An example job created from terraform"
+      description = "An example job created from terraform"
     })
 }
