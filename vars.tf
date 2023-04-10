072a102fb3ff75330d2ba3a8d0dd4dddc11ef086
TARAK <tarakaramtirumala@gmail.com>
2023-04-10 12:21:53 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -4,7 +4,13 @@ variable "folders" {
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-infra" }
-
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "frontend", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "cart", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "catalogue", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "user", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "shipping", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    { name = "payment", folder = "Ci-pipeline", repo_url = "https://github.com/sriharitirumala/roboshop-Ci-pipeline" },
+    
   ]
 }    
