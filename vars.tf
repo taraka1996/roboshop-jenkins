79d5b6204eef4cf46788e3b2188300b86e27cd98
TARAK <tarakaramtirumala@gmail.com>
2023-04-10 07:59:30 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -4,7 +4,7 @@ variable "folders" {
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
-
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/taraka1996/roboshop-infra" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/taraka1996/frontend" },
   ]
 }   
