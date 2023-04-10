52ad785e0592792b88cdddf8e776c02d7d78dd64
TARAK <tarakaramtirumala@gmail.com>
2023-04-10 10:32:31 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "jenkins_folder" "folders" {
   count = length(var.folders)
-  name  = element(var.folders, count.index)
+  name =  element(var.folders, count.index )
 }
 
 resource "jenkins_job" "job" {
-  depends_on = [jenkins_folder.folders]
-
-  count  = length(var.jobs)
-  name   = lookup(element(var.jobs, count.index), "name", null)
-  folder = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
-
+  count    = length(var.jobs)
+  name     = lookup(element(var.jobs, count.index), "name", null)
+  folder   = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
-    name     = lookup(element(var.jobs, count.index), "name", null)
   })
-} 
+
+  lifecycle {
+    ignore_changes = [ template ]
+  }
+}
--- vars.tf
+++ vars.tf
@@ -4,7 +4,6 @@ variable "folders" {
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/taraka1996/roboshop-infra" },
-    
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sriharitirumala/roboshop-infra" }
   ]
-}   
+}
