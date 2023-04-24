ca526d256651928b875d994fadded7cbd4f06f7d
TARAK <tarakaramtirumala@gmail.com>
2023-04-24 16:59:35 +0530

    chnages

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "jenkins_job" "s-job" {
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url = lookup(element(var.s-jobs, count.index), "repo_url", null)
     name = lookup(element(var.s-jobs, count.index), "name", null)
+    filename = lookup(element(var.s-jobs, count.index), "filename", null)
   })
 
     lifecycle {
--- vars.tf
+++ vars.tf
@@ -16,8 +16,9 @@ variable "m-jobs" {
 
 variable "s-jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/taraka1996/roboshop-infra" },
-
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/taraka1996/roboshop-infra", filename = "Jenkinsfile" },
+    { name = "deployment", folder = "Infra", repo_url = "https://github.com/taraka1996/roboshop-ansible", filename = "jenkinsfile-deployment" },
+    
   ]
 
 }  
